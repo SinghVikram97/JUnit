c29ada182a840daf8f63c368133a5c0c23922e23
Vikram <vbedi@expediagroup.com>
2021-05-18 11:20:05 +0530

    Added more tests

--- src/test/java/stringhelper/StringHelperTest.java
+++ src/test/java/stringhelper/StringHelperTest.java
@@ -12,24 +12,46 @@ public class StringHelperTest {
 
     // name_condition(in which are testing)
     @Test
-    public void testTruncateAInFirst2Positions_AInFirst2Positions() {
+    public void truncateAInFirst2Positions_AInFirst2Positions() {
         // Expected, Actual
         assertEquals("CD",stringHelper.truncateAInFirst2Positions("AACD"));
     }
 
     @Test
     // One condition should be checked per function
-    public void testTruncateAInFirst2Positions_AInFirstPosition(){
+    public void truncateAInFirst2Positions_AInFirstPosition(){
         assertEquals("CD",stringHelper.truncateAInFirst2Positions("ACD"));
     }
 
     @Test
-    public void testTruncateAInFirst2Positions_ANotInFirst2Positions(){
+    public void truncateAInFirst2Positions_ANotInFirst2Positions(){
         assertEquals("CDAA",stringHelper.truncateAInFirst2Positions("CDAA"));
     }
 
     @Test
-    public void testTruncateAInFirst2Positions_ANotInString(){
+    public void truncateAInFirst2Positions_ANotInString(){
         assertEquals("CDEF",stringHelper.truncateAInFirst2Positions("CDEF"));
+    }
+
+    // ABCD => false , ABAB => true , AB => true , A => false
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame_BasicNegativeScenario(){
+        assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame_BasicPositiveScenario(){
+        assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame_StringLengthTwo(){
+        assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("AB"));
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame_StringLengthOne(){
+        assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("A"));
     }
 }
