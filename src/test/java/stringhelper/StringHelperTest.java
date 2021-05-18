8dcc7fba28c06c749e63326244d4ce6e40e7a9f8
Vikram <vbedi@expediagroup.com>
2021-05-18 10:01:57 +0530

    refactored and added more test

--- src/test/java/stringhelper/StringHelperTest.java
+++ src/test/java/stringhelper/StringHelperTest.java
@@ -10,11 +10,15 @@ public class StringHelperTest {
 
     @Test
     public void truncateAInFirst2Positions() {
+
         // AACD => CD  , ACD => CD , CDEF => CDEF  , CDAA => CDAA
 
-        String actualValue = stringHelper.truncateAInFirst2Positions("AACD");
-        String expectedValue="CD";
-        assertEquals(expectedValue,actualValue);
+        // Expected, Actual
+        assertEquals("CD",stringHelper.truncateAInFirst2Positions("AACD"));
+        assertEquals("CD",stringHelper.truncateAInFirst2Positions("ACD"));
+        assertEquals("CDEF",stringHelper.truncateAInFirst2Positions("CDEF"));
+        assertEquals("CDAA",stringHelper.truncateAInFirst2Positions("CDAA"));
+
 
     }
 
