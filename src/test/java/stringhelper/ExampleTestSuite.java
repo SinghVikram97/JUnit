0244baef5dadc4904c78ad4521914c4c0b9c4f27
Vikram <vbedi@expediagroup.com>
2021-05-18 17:15:13 +0530

    Test suites

---
+++ src/test/java/stringhelper/ExampleTestSuite.java
@@ -0,0 +1,9 @@
+package stringhelper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({StringHelperTest.class,BeforeAfterTest.class})
+public class ExampleTestSuite {
+}
